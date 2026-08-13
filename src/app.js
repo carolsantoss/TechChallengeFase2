211a0af98b6656bffef26f2162e7c116468a6ec2
aamekst <mieko.alana@gmail.com>
2026-08-13 19:36:54 -0300

    build: add cors

--- src/app.js
+++ src/app.js
@@ -1,24 +1,39 @@
 const express = require('express');
+const cors = require('cors');
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./config/swagger');
+
 const sequelize = require('./config/database');
+
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 
+
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 
 app.use(express.json());
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 
 app.get('/', (req, res) => {
   res.redirect('/docs');
 });
 
+
 app.use(postRoutes);
 app.use(userRoutes);
 app.use(authRoutes);
+
 module.exports = app;
-
-
